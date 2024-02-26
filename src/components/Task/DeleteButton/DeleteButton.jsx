f9b06233cd21bb76158b5a3efcbd556d91002a4e
Daniil Sivtsov <danilklin17@gmail.com>
2024-02-26 16:39:27 +0300

    add memo in DeleteButton

--- src/components/Task/DeleteButton/DeleteButton.jsx
+++ src/components/Task/DeleteButton/DeleteButton.jsx
@@ -1,13 +1,16 @@
+import { memo, useCallback } from "react";
 import cx from "classnames";
 
 import s from "./DeleteButton.module.scss";
 
-const DeleteButton = ({ className, id, setList }) => {
-  const handlerClick = () => {
+const DeleteButton = memo(({ className, id, setList }) => {
+  const handlerClick = useCallback(() => {
     setList((prev) => prev.filter((el) => el.id !== id));
-  };
+  }, [id, setList]);
 
   return <button className={cx(s.root, className)} onClick={handlerClick} />;
-};
+});
+
+DeleteButton.displayName = "DeleteButton";
 
 export default DeleteButton;
